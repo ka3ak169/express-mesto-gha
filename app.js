2a24ec77ccffcdd602626ce6434af16ad0be8031
Aleksey Kazakov <ka3akne@mail.ru>
2023-06-20 01:07:33 +0400

    14 спринт прохождение тестов 16

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 require('dotenv').config();
 const userRouter = require('./routers/users');
 const cardRouter = require('./routers/cards');
@@ -25,7 +26,13 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
 
 // Подключение к серверу MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
