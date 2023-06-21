4cb8536d347f4782d665dd4c218a9de2a5f296e4
Aleksey Kazakov <ka3akne@mail.ru>
2023-06-22 01:51:40 +0400

    вторая попытка проверка cards

--- routers/cards.js
+++ routers/cards.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-useless-escape */
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const {
@@ -9,9 +11,16 @@ const {
   deleteCardLike,
 } = require('../controllers/cards');
 
+const cardValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(https?:\/\/)(www\.)?([a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/),
+  }),
+});
+
 router.get('/cards', authMiddleware, getCards);
 
-router.post('/cards', authMiddleware, postCards);
+router.post('/cards', authMiddleware, cardValidation, postCards);
 
 router.delete('/cards/:cardId', authMiddleware, deleteCards);
 
